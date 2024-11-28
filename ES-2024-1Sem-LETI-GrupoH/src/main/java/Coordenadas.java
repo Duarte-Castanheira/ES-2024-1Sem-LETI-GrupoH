a2617e30f3407d86b09686225117d17f880ab770
Duarte_Castanheira <dacaa1@iscte-iul.pt>
2024-11-28 14:28:48 +0000

    Classe coordenadas

---
+++ ES-2024-1Sem-LETI-GrupoH/src/main/java/Coordenadas.java
@@ -0,0 +1,17 @@
+public class Coordenadas {
+    double x;
+    double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public Coordenadas(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
