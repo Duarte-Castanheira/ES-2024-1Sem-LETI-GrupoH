d06fffe96288d284ff1072883dbee8169987936b
Afonso_Maia <asmaa1@iscte-iul.pt>
2024-12-06 14:37:19 +0000

    Main

--- ES-2024-1Sem-LETI-GrupoH/src/main/java/Main.java
+++ ES-2024-1Sem-LETI-GrupoH/src/main/java/Main.java
@@ -1,10 +1,10 @@
-import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Terreno> ListaTerrenos = Ler_DataBase.ReadFile("Madeira-Moodle-1.1.csv");
+        Map<Integer, Terreno> ListaTerrenos = Ler_DataBase.ReadFile("Madeira-Moodle-1.1.csv");
         Graph.CreateGraph(ListaTerrenos);
     }
 }
